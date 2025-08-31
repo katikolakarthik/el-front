94a26a54c7d8f4a97e5ea55ce98d19b2c91c23f6
KATIKOLA KARTHIK <katikolakarthik@gmail.com>
2025-08-31 21:45:39 +0530

    Update CRSWidget.jsx

--- src/components/CRSWidget.jsx
+++ src/components/CRSWidget.jsx
@@ -3,16 +3,23 @@ import { ExternalLink } from 'lucide-react';
 import './CRSWidget.css';
 
 const CRSWidget = () => {
-  const handleClick = () => {
-    window.open('https://3mhis.lccc.wy.edu/launchCRS.html', '_blank');
-  };
-
   return (
-    <div className="crs-widget" onClick={handleClick}>
-      <div className="crs-icon">
-        <ExternalLink size={20} />
+    <div className="crs-widget">
+      <div className="crs-header">
+        <div className="crs-icon">
+          <ExternalLink size={20} />
+        </div>
+        <span className="crs-label">3M CODING</span>
       </div>
-      <span className="crs-label">3M CODING</span>
+
+      {/* Embedded website */}
+      <iframe
+        src="https://3mhis.lccc.wy.edu/launchCRS.html"
+        title="3M Coding System"
+        width="100%"
+        height="600px"
+        style={{ border: "none", borderRadius: "8px" }}
+      />
     </div>
   );
 };
